4caf97321e8088a0aecf028bd7cb80aca3736bfa
Josip <popovicjosip867@gmail.com>
2023-09-01 14:20:27 +0200

    Small changes

--- src/Form/InputMessage.jsx
+++ src/Form/InputMessage.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { ErrorMessage } from '@hookform/error-message';
 import { Typography } from '@mui/material';
 
-const InputMessage = ({ errors, name, helperText }) => {
+const InputMessage = ({ errors, name, helperText, errMsgProps }) => {
     return (
         (Object.keys(errors).length !== 0 && errors[name] && (
             <ErrorMessage
                 errors={errors}
                 name={name}
                 render={({ message }) => (
-                    <Typography component="span" variant="caption" color="inherit">
+                    <Typography component="span" variant="caption" color="inherit" {...errMsgProps}>
                         {message}
                     </Typography>
                 )}
