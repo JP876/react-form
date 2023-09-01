f64c9b483d0cef78ec2242c5c970af84b50514d2
Josip <popovicjosip867@gmail.com>
2023-09-01 14:39:03 +0200

    Small change with checkbox input

--- src/Form/Inputs/CheckboxRender.jsx
+++ src/Form/Inputs/CheckboxRender.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Checkbox, FormControlLabel, FormHelperText, Stack } from '@mui/material';
+import { Box } from '@mui/system';
 
 const CheckboxRender = ({ onChange, value, label, helperText, inputProps }) => {
     return (
         <Stack id="updateForm_checkbox" direction="row" justifyContent="center" mt={0.4}>
-            <FormControlLabel
-                control={
-                    <Checkbox
-                        color="primary"
-                        {...inputProps}
-                        onChange={(e) => {
-                            onChange(e.target.checked);
-                        }}
-                        checked={typeof value === 'boolean' && value}
-                    />
-                }
-                label={label || inputProps?.label || ''}
-            />
-            <FormHelperText>{helperText}</FormHelperText>
+            <Box>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            color="primary"
+                            {...inputProps}
+                            onChange={(e) => {
+                                onChange(e.target.checked);
+                            }}
+                            checked={typeof value === 'boolean' && value}
+                        />
+                    }
+                    label={label || inputProps?.label || ''}
+                />
+                <FormHelperText>{helperText}</FormHelperText>
+            </Box>
         </Stack>
     );
 };
